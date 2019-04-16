5e00f3eee971dde7683f3409c636ef9e27b9cb80
tom <tom@exaple.com>
2019-04-16 15:36:14 +0800

    配置url地址，创建主页页面

--- djangodemo/booktest/views.py
+++ djangodemo/booktest/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("index")
+
+
+def detail(request, id):
+    return HttpResponse("detail %s" % (id,))
+
+
+def book_list(request, id):
+    return HttpResponse("detail %s" % (id,))
